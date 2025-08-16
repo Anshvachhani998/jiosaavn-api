6e9820657983ec4eab13c1e2a02c4ca742e0124f
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-08-16 15:05:16 +0530

    Update get-artist-songs.use-case.ts

--- src/modules/artists/use-cases/get-artist-songs/get-artist-songs.use-case.ts
+++ src/modules/artists/use-cases/get-artist-songs/get-artist-songs.use-case.ts
@@ -7,6 +7,7 @@ import type { z } from 'zod'
 
 export interface GetArtistSongsArgs {
   artistId: string
+  page: number
   sortBy: 'popularity' | 'latest' | 'alphabetical'
   sortOrder: 'asc' | 'desc'
 }
@@ -14,38 +15,28 @@ export interface GetArtistSongsArgs {
 export class GetArtistSongsUseCase implements IUseCase<GetArtistSongsArgs, z.infer<typeof ArtistSongModel>> {
   constructor() {}
 
-  async execute({ artistId, sortOrder, sortBy }: GetArtistSongsArgs) {
-    const allSongs: { id: string, name: string }[] = []
-    let currentPage = 1
-    let totalPages = 1
+  async execute({ artistId, page, sortOrder, sortBy }: GetArtistSongsArgs) {
+    const { data } = await useFetch<z.infer<typeof ArtistSongAPIResponseModel>>({
+      endpoint: Endpoints.artists.songs,
+      params: {
+        artistId,
+        page,
+        sort_order: sortOrder,
+        category: sortBy
+      }
+    })
 
-    do {
-      const { data } = await useFetch<z.infer<typeof ArtistSongAPIResponseModel>>({
-        endpoint: Endpoints.artists.songs,
-        params: {
-          artistId,
-          page: currentPage,
-          sort_order: sortOrder,
-          category: sortBy
-        }
-      })
+    if (!data) throw new HTTPException(404, { message: 'artist songs not found' })
 
-      if (!data) throw new HTTPException(404, { message: 'artist songs not found' })
-
-      // Sirf id + name extract karo
-      allSongs.push(...data.topSongs.songs.map(song => ({
-        id: song.id,
-        name: song.name
-      })))
-
-      // Total pages calculate karo
-      totalPages = Math.ceil(data.topSongs.total / data.topSongs.songs.length)
-      currentPage++
-    } while (currentPage <= totalPages)
+    // sirf music id + name
+    const songs = data.topSongs.songs.map(song => ({
+      musicId: song.id,
+      name: song.name
+    }))
 
     return {
-      total: allSongs.length,
-      songs: allSongs
+      total: data.topSongs.total, // total songs count
+      songs
     }
   }
 }
